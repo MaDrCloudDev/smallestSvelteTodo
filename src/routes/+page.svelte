5d4ec2015c2b7710fce908a0f3dedc3e851db166
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2025-08-07 12:35:07 -0700

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	let todos = $state([{ text: 'Fly to the moon', done: false }]),
+		done = $derived(todos.filter((t) => t.done).length),
+		onchange = (e) => todos.push({ text: e.target.value, done: false }) && (e.target.value = '');
+</script>
+
+<input {onchange} />
+{#each todos as todo}
+	<div>
+		<input bind:value={todo.text} />
+		<input type="checkbox" bind:checked={todo.done} />
+	</div>
+{/each}
+Done: {done}
